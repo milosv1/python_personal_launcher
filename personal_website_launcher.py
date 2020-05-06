56e42e97659ddb8bf6434c2ff304f70b0b037774
Milos <milosvuksanovic53@gmail.com>
2020-05-06 20:08:16 +1000

    added extra comments

--- personal_website_launcher.py
+++ personal_website_launcher.py
@@ -10,11 +10,18 @@ google_ToastNotification = ToastNotifier()
 #get input from user through prompt
 website_selectInput = input("1 for YouTube \n 2 for Google")
 
+#if the user enters 1
 if website_selectInput == '1':
+    #show desktop notification - of said opening website
     youtube_ToastNotification.show_toast("Opening YouTube", f"{website_selectInput} YouTube Selected - Opening YouTube")
+    #using webbrowser library open selected webbrowser
     webbrowser.open("http://www.youtube.com")
+#else if user inputs 2    
 elif website_selectInput == '2':
+    #notify the user using the show_toast from import ToastNotifier of said opening website
     google_ToastNotification.show_toast("Opening Google",f"{website_selectInput} Google selected - Opening Google")   
+    #open using webbrowser.open() ..
     webbrowser.open("http://www.google.com")
+    #else if user inputs anything else - show they can only use 1 or 2
 else:
     print("input can only be 1 OR 2")     
